7faca39ffee2dd0fec1dc3698e0e7f6165f56f43
carinama1 <jarpani30@gmail.com>
2021-02-04 12:28:56 +0700

    d

--- webpack.server.js
+++ webpack.server.js
@@ -3,7 +3,7 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "development",
-  entry: ["react-hot-loader/patch", "./server/server.js"],
+  entry: "./server/server.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "server.bundle.js",
